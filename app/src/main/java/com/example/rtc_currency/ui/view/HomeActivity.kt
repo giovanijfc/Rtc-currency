0d311df263d653c774da905506f8f762f70eb6a5
Giovani Chiodi <giovanichiodi@android-d783193a71e88a3.ioasys>
2020-02-28 15:24:01 -0300

    Adds new screen home and start this

---
+++ app/src/main/java/com/example/rtc_currency/ui/view/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rtc_currency.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rtc_currency.R
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/rtc_currency/ui/view/SplashActivity.kt
+++ app/src/main/java/com/example/rtc_currency/ui/view/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rtc_currency.ui.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rtc_currency.R
@@ -8,6 +9,12 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        Thread {
+            val homeIntent = Intent(this, HomeActivity::class.java)
+            Thread.sleep(1000)
+            startActivity(homeIntent)
+        }.start()
     }
 }
