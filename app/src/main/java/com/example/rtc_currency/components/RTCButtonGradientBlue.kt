18b619d15e4c0905b21e4608138a907f693707c7
giovanijfc <giovanijfc@gmail.com>
2020-03-01 13:43:19 -0300

    Adds reusuble component button

---
+++ app/src/main/java/com/example/rtc_currency/components/RTCButtonGradientBlue.kt
@@ -0,0 +1,17 @@
+package com.example.rtc_currency.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.example.rtc_currency.R
+
+class RTCButtonGradientBlue(context: Context, attrs: AttributeSet) :
+    AppCompatButton(context, attrs) {
+
+    init {
+        setTextColor(ContextCompat.getColor(context, R.color.white))
+        setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.shape_button_gradient_blue_rounded))
+    }
+}
