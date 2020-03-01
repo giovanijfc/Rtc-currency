81a7f23c8e1f84808a6e6e2e087865355ab23f41
giovanijfc <giovanijfc@gmail.com>
2020-03-01 18:16:26 -0300

    Adds title in mid and recorganized layout

--- app/src/main/java/com/example/rtc_currency/ui/view/BaseActivity.kt
+++ app/src/main/java/com/example/rtc_currency/ui/view/BaseActivity.kt
@@ -2,11 +2,13 @@ package com.example.rtc_currency.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.rtc_currency.R
+import kotlinx.android.synthetic.main.component_toolbar_search.view.*
 
 open class BaseActivity : AppCompatActivity() {
 
     fun setToolbar(toolbar: Toolbar, textTitle: String) {
-        toolbar.title = textTitle
+        toolbar.text_title.text = textTitle
         setSupportActionBar(toolbar)
     }
 }
