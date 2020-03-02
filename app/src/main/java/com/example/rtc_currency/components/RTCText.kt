2429f2e2fae845c46bad85b9417ab90a6aee8d95
giovanijfc <giovanijfc@gmail.com>
2020-03-01 23:25:16 -0300

    Fixes layout

--- app/src/main/java/com/example/rtc_currency/components/RTCText.kt
+++ app/src/main/java/com/example/rtc_currency/components/RTCText.kt
@@ -3,6 +3,12 @@ package com.example.rtc_currency.components
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.rtc_currency.R
 
 class RTCText(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
+
+    init {
+        setTextColor(ContextCompat.getColor(context, R.color.white))
+    }
 }
--- app/src/main/java/com/example/rtc_currency/components/RTCTextBold.kt
+++ app/src/main/java/com/example/rtc_currency/components/RTCTextBold.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.rtc_currency.R
 
 class RTCTextBold(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
 
     init {
         setTypeface(Typeface.SANS_SERIF, Typeface.BOLD)
+        setTextColor(ContextCompat.getColor(context, R.color.white))
     }
 }
